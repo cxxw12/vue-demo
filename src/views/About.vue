85aedfa623348059211a333c74945998b7a80049
cxxw12 <1354876428@qq.com>
2022-04-18 20:44:00 +0800

    el-select 多选框，默认值不可删除实现方法。

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,6 +10,19 @@
   2222221111111111111111111 11111111111111111 11111111111111111111
 		11111111111111111111111 111111111111111111122222
 		</p>
+     <el-form  :model="form" label-width="150px">
+      <el-form-item label="品类：" prop="Type">
+        <el-select  multiple  v-model="form.Type" @remove-tag="handleMaterialTypeRemoveTag" @change="changeFn" >
+          <el-option
+            :disabled="dType.includes(item.value)"
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+     </el-form>
   </div>
 </template>
 <script>
@@ -17,7 +30,28 @@ import myMixin from "@/assets/mixin.js";
 export default {
   mixins: [myMixin],
   data() {
-    return {};
+    return {
+      form:{
+          Type:['选项1']
+        },
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        dType:[]
+    };
   },
   mounted() {
     console.log(this.mixinNum)
@@ -35,6 +69,8 @@ export default {
     this.EventBus.$on(["test","test2"], (val) => {
       console.log(val);
     });
+    this.getdefMaterialType()
+    this.changeFn()
     // this.EventBus.$on("test", (val) => {
     //   console.log(val);
     // });
@@ -43,16 +79,29 @@ export default {
     mouserover() {
       console.log("111");
     },
+    handleMaterialTypeRemoveTag(value) {
+      const index = this.dType.indexOf(value)
+      console.log(index)
+        if (index !== -1) {
+          this.form.Type.splice(index, 0, value)
+        }
+      },
+      getdefMaterialType(){
+        this.dType = this.form.Type
+      },
+     changeFn(){
+        console.log(this.form.Type)
+      }
   },
 };
 </script>
 <style lang="scss" scoped>
-div {
-  width: 100px;
-  height: 100px;
-  background: #000;
-  margin-top: 10px;
-}
+// div {
+//   width: 100px;
+//   height: 100px;
+//   background: #000;
+//   margin-top: 10px;
+// }
 p {
   position: relative;
   line-height: 20px;
